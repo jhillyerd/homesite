5c82f17ab660242bf668a7cc4559380a7e7665a7
James Hillyerd <james@hillyerd.com>
2023-03-11 10:32:22 -0800

    update flake deps

--- flake.nix
+++ flake.nix
@@ -2,10 +2,9 @@
   description = "Home intranet website";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
 
     flake-utils.url = "github:numtide/flake-utils";
-    flake-utils.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = { self, nixpkgs, flake-utils }:
