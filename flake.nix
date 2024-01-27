10b85712822a89d64bfc083039958dc1c98b1fe8
James Hillyerd <james@hillyerd.com>
2024-01-26 17:39:20 -0800

    update flake deps

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My homelab intranet website";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
 
     flake-utils.url = "github:numtide/flake-utils";
 
